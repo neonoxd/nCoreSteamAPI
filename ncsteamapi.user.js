01355e98961c95a085b5bf212fa0d5af748002e1
neonoxd <neonoxd14@gmail.com>
2018-12-19 23:24:26 +0100

    name convention fix

--- ncsteamapi.user.js
+++ ncsteamapi.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         nCore steamAPI Helper
 // @namespace    by dbw
-// @version      0.7.2
+// @version      0.7.3
 // @description  nCore x SteamAPI
 // @author       neonoxd aka dbw
 // @homepageURL      https://github.com/neonoxd/nCoreSteamAPI/
 // @updateURL        https://raw.githubusercontent.com/neonoxd/nCoreSteamAPI/master/ncsteamapi.meta.js
-// @downloadURL      https://raw.githubusercontent.com/neonoxd/nCoreSteamAPI/master/ncsteamapi.script.js
+// @downloadURL      https://raw.githubusercontent.com/neonoxd/nCoreSteamAPI/master/ncsteamapi.user.js
 // @include        https://ncore.cc/*
 // @include        https://ncore.nu/*
 // @grant        none
