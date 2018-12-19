b6b9cdb9799695206021e009048d4b2c0e5df754
neonoxd <neonoxd14@gmail.com>
2018-12-19 22:55:36 +0100

    Create ncsteamapi.meta.js

---
+++ ncsteamapi.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         nCore steamAPI Helper
+// @namespace    by dbw
+// @version      0.7
+// @description  nCore x SteamAPI
+// @author       neonoxd aka dbw
+// @include        https://ncore.cc/*
+// @include        https://ncore.nu/*
+// @grant        none
+// ==/UserScript==
