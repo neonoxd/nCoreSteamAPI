11837a5a0ca0ed8933777b075f4a032c4c8c2a13
neonoxd <neonoxd14@gmail.com>
2018-12-19 22:58:46 +0100

    Update ncsteamapi.script.js

--- ncsteamapi.script.js
+++ ncsteamapi.script.js
@@ -4,6 +4,9 @@
 // @version      0.7
 // @description  nCore x SteamAPI
 // @author       neonoxd aka dbw
+// @homepageURL      https://github.com/neonoxd/nCoreSteamAPI/
+// @updateURL        https://raw.githubusercontent.com/neonoxd/nCoreSteamAPI/master/ncsteamapi.meta.js
+// @downloadURL      https://raw.githubusercontent.com/neonoxd/nCoreSteamAPI/master/ncsteamapi.script.js
 // @include        https://ncore.cc/*
 // @include        https://ncore.nu/*
 // @grant        none
